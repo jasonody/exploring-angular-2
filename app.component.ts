050bb6045bb9915ee69dc642926465d1a2d06ed6
Jason Ody <jasonody@users.noreply.github.com>
2016-08-30 21:58:23 -0700

    Added app component

---
+++ app.component.ts
@@ -0,0 +1,9 @@
+import  {Component} from 'angular2/core';
+
+@Component({
+  selector: 'main-app',
+  template: '<h1>My main app component</h1>'
+})
+export class AppComponent {
+  constructor() {}
+}
